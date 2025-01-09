101a638b0b83fe98051e0c36c03818a0ec997f1f
Marcus Ikdal <marcus.ikdal@gmail.com>
2025-01-09 13:27:36 +0100

    started implementing TodoList

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,44 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.ArrayList;
 
+public class TodoList {
+    private ArrayList<Task> tasks = new ArrayList<>();
+
+    ArrayList<Task> addTask(Task t) {
+        tasks.add(t);
+        return tasks;
+    }
+
+    void completeTask(String taskName) {
+
+    }
+
+    ArrayList<Task> getAll() {
+        return tasks;
+    }
+
+    ArrayList<Task> getCompleted() {
+        return tasks;
+    }
+
+    ArrayList<Task> getIncompleted() {
+        return tasks;
+    }
+
+    ArrayList<Task> getAllAscending() {
+        return tasks;
+    }
+
+    ArrayList<Task> getAllDescending() {
+        return tasks;
+    }
+
+    boolean search(String taskName) {
+        return false;
+    }
+
+    Task removeTask(String taskName) {
+        return null;
+    }
 }
