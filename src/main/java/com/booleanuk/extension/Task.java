de7c58f888b86d88140b7ccffe48332f630b279f
Marcus Ikdal <marcus.ikdal@gmail.com>
2025-01-09 14:48:14 +0100

    implemented extension for Task

--- src/main/java/com/booleanuk/extension/Task.java
+++ src/main/java/com/booleanuk/extension/Task.java
@@ -1,12 +1,22 @@
 package com.booleanuk.extension;
 
+import java.time.LocalDateTime;
+
 public class Task {
     private boolean completed = false;    
     private String name;
+    private int id = -1;
+    private LocalDateTime timeCreated = LocalDateTime.now();
 
     public Task(String name) {
         this.name = name;
     }
+
+    public Task(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
 
     void complete() {
         completed = true;
@@ -18,5 +28,21 @@ public class Task {
 
     String getName() {
         return name;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    void changeName(String newName) {
+        name = newName;
+    }
+
+    void setStatus(boolean completed) {
+        this.completed = completed;
+    }
+
+    LocalDateTime getTimeCreated() {
+        return timeCreated;
     }
 }
